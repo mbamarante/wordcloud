aaca9b4954571ec6963153c19c86a4286610215f
maicon.amarante <maicon.amarante@iffarroupilha.edu.br>
2020-10-10 08:59:52 -0300

    Inclui http na exibição do link

--- plugins/Groups/templates/BreakoutRooms/build.php
+++ plugins/Groups/templates/BreakoutRooms/build.php
@@ -4,9 +4,9 @@
 
     foreach($groups as $gk => $gv) {
         if ($class != 'geral') {
-            $link = "<a href=\"http://".$class ."grupo". $gk ."treinamentopfle.fergs.org.br" . "\">" .$class ."grupo". $gk ."treinamentopfle.fergs.org.br</a>";
+            $link = "<a href=\"http://".$class ."grupo". $gk ."treinamentopfle.fergs.org.br" . "\">http://" .$class ."grupo". $gk ."treinamentopfle.fergs.org.br</a>";
         } else {
-            $link = "<a href=\"http://grupo". $gk ."treinamentopfle.fergs.org.br" . "\">grupo". $gk ."treinamentopfle.fergs.org.br</a>";
+            $link = "<a href=\"http://grupo". $gk ."treinamentopfle.fergs.org.br" . "\">http://grupo". $gk ."treinamentopfle.fergs.org.br</a>";
         }
         echo "GRUPO $gk - $link:<BR>";
         foreach($gv as $mk => $mv) {
